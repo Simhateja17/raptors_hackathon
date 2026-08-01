a0b51eab394b2bc716b43a408aef6659420c8501
Poojitha Gobburu <poojithagobburu@gmail.com>
2026-08-01 19:28:03 +0530

    Add Poojitha native test harness

---
+++ rust/tests/poojitha.rs
@@ -0,0 +1,14 @@
+#[path = "algorithms/hamming.rs"]
+mod hamming;
+#[path = "algorithms/jaccard.rs"]
+mod jaccard;
+#[path = "algorithms/sorensen.rs"]
+mod sorensen;
+#[path = "algorithms/tversky.rs"]
+mod tversky;
+#[path = "algorithms/cosine.rs"]
+mod cosine;
+#[path = "algorithms/bag.rs"]
+mod bag;
+#[path = "algorithms/rle_ncd.rs"]
+mod rle_ncd;
